71ced141e7522c3dbc11fbdb710df3ba7cfd7bb2
Mikhail Mamayeu <Mikhail.Mamayeu@bglgroup.co.uk>
2021-02-02 15:26:31 +0300

    Add configs for micro frontend applications

---
+++ src/config/micro-frontends.js
@@ -0,0 +1,14 @@
+export default {
+  registration: {
+    name: 'credit-report.registration.poc',
+    host: 'http://127.0.0.1:3001',
+    script: 'main.js',
+    render: 'renderRegistration',
+  },
+  summaryReport: {
+    name: 'credit-report.summary-report.poc',
+    host: 'http://127.0.0.1:3002',
+    script: 'main.js',
+    render: 'renderSummaryReport',
+  },
+};
