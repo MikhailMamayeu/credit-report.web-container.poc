37c1317d1b6a621f69a5a451f26c435bf97d8661
Mikhail Mamayeu <Mikhail.Mamayeu@bglgroup.co.uk>
2021-02-02 15:27:16 +0300

    Use MicroFrontend component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 
+import MicroFrontend from '../MicroFrontend';
+
+import microFrontends from '../../config/micro-frontends';
+
 import './App.css';
 
 const App = () => (
-  <header className="app__header">
-    <h1>Credit Report Web Container</h1>
-  </header>
+  <>
+    <header className="app__header">
+      <h1>Credit Report Web Container</h1>
+    </header>
+    <main>
+      <section id={`${microFrontends.registration.name}-container`}>
+        <MicroFrontend config={microFrontends.registration} />
+      </section>
+      <section id={`${microFrontends.summaryReport.name}-container`}>
+        <MicroFrontend config={microFrontends.summaryReport} />
+      </section>
+    </main>
+  </>
 );
 
 export default App;
