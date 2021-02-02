09a3e690de61668e638d4c6cbfe51c1cabd95b89
Mikhail Mamayeu <Mikhail.Mamayeu@bglgroup.co.uk>
2021-02-02 17:58:41 +0300

    Replace 'webpack-plugin-serve' with beta version of 'webpack-dev-server'

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,25 +1,23 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const { WebpackPluginServe } = require('webpack-plugin-serve');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: ['./src/index.js', 'webpack-plugin-serve/client'],
   output: {
     filename: 'static/[name].js',
     publicPath: '/',
+  },
+  devServer: {
+    host: '127.0.0.1',
+    port: 3000,
+    static: ['dist/static'],
+    open: true,
+    historyApiFallback: true,
   },
   devtool: 'eval-source-map',
   plugins: [
     new CleanWebpackPlugin({
       cleanStaleWebpackAssets: false,
-    }),
-    new WebpackPluginServe({
-      host: '127.0.0.1',
-      port: 3000,
-      static: ['dist', 'dist/static'],
-      open: true,
-      historyFallback: true,
     }),
     new ReactRefreshWebpackPlugin(),
     new MiniCssExtractPlugin({
