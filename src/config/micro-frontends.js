6dca677a30593c77516f20e8081460ffbc7ed4d9
Mikhail Mamayeu <Mikhail.Mamayeu@bglgroup.co.uk>
2021-02-04 15:23:21 +0300

    Use different micro frontends host names in dev and prod mode

--- src/config/micro-frontends.js
+++ src/config/micro-frontends.js
@@ -1,14 +1,20 @@
 export default {
   registration: {
     name: 'credit-report.registration.poc',
-    host: 'http://127.0.0.1:3001',
+    host:
+      window.environment === 'development'
+        ? 'https://credit-report-registration-poc.dev.ctmers.io:3001'
+        : 'http://127.0.0.1:3001',
     script: 'main.js',
     styles: 'main.css',
     render: 'renderRegistration',
   },
   summaryReport: {
     name: 'credit-report.summary-report.poc',
-    host: 'http://127.0.0.1:3002',
+    host:
+      window.environment === 'development'
+        ? 'https://credit-report-summary-report-poc.dev.ctmers.io:3002'
+        : 'http://127.0.0.1:3002',
     script: 'main.js',
     styles: 'main.css',
     render: 'renderSummaryReport',
