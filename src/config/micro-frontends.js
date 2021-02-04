69e553736d4c97167a49882d74e8ca6348258629
Mikhail Mamayeu <Mikhail.Mamayeu@bglgroup.co.uk>
2021-02-04 15:55:23 +0300

    Supress ESLint on unused environment variable

--- src/config/micro-frontends.js
+++ src/config/micro-frontends.js
@@ -2,7 +2,8 @@ export default {
   registration: {
     name: 'credit-report.registration.poc',
     host:
-      window.environment === 'development'
+      // eslint-disable-next-line no-undef
+      environment === 'development'
         ? 'https://credit-report-registration-poc.dev.ctmers.io:3001'
         : 'http://127.0.0.1:3001',
     script: 'main.js',
@@ -12,7 +13,8 @@ export default {
   summaryReport: {
     name: 'credit-report.summary-report.poc',
     host:
-      window.environment === 'development'
+      // eslint-disable-next-line no-undef
+      environment === 'development'
         ? 'https://credit-report-summary-report-poc.dev.ctmers.io:3002'
         : 'http://127.0.0.1:3002',
     script: 'main.js',
