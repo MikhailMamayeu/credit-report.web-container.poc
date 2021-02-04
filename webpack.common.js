a01000c646a2ec9ac2ac94c611e2d0983ee67887
Mikhail Mamayeu <Mikhail.Mamayeu@bglgroup.co.uk>
2021-02-04 15:22:32 +0300

    Use DefinePlugin to pass through env variable

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -34,6 +35,7 @@ module.exports = {
       title: 'Credit Report Web Container',
       environment: process.env.NODE_ENV,
     }),
+    new webpack.DefinePlugin({ environment: process.env.NODE_ENV }),
   ],
   resolve: {
     extensions: ['.js', '.jsx'],
