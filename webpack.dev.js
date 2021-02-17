fabd853d8666a87395c28a8cadbed51ea2696323
Mikhail Mamayeu <Mikhail.Mamayeu@bglgroup.co.uk>
2021-02-17 12:38:59 +0300

    Disable HMR as it does not work with module federation

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,4 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
@@ -11,6 +10,8 @@ module.exports = {
     host: 'credit-report-web-container-poc.dev.ctmers.io',
     port: 3000,
     static: ['dist/static'],
+    hot: false,
+    liveReload: true,
     open: true,
     historyApiFallback: true,
   },
@@ -19,7 +20,6 @@ module.exports = {
     new CleanWebpackPlugin({
       cleanStaleWebpackAssets: false,
     }),
-    new ReactRefreshWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: 'static/[name].css',
     }),
